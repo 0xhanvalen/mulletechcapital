eeaf58267f5d4deef27fe685f153a850408b341b
Han Valen <han@0xhanvalen.com>
2022-11-16 08:30:55 -0800

    mulletech

---
+++ src/routes/[...404].jsx
@@ -0,0 +1,14 @@
+import { Title } from "solid-start";
+import { HttpStatusCode } from "solid-start/server";
+export default function NotFound() {
+  return (
+    <main>
+      <Title>Not Found</Title>
+      <HttpStatusCode code={404} />
+      <h1>Page Not Found</h1>
+      <p>
+        <a href="https://www.mulletechcapital.com">Return Home</a>
+      </p>
+    </main>
+  );
+}
---
+++ src/routes/index.jsx
@@ -0,0 +1,18 @@
+import { Title } from "solid-start";
+export default function Home() {
+  return (
+    <main>
+      <Title>Mulletech Capital</Title>
+      <div className="center">
+        <h1>Mulletech Capital Inc.</h1>
+        <h2><i>"Don't worry about it"</i></h2>
+        <p className="address">
+          1139 West Georgia Street
+          <br />
+          Vancouver, British Columbia, Canada
+        </p>
+        <p style={{textAlign: `center`}}><b>Offical Home Page</b></p>
+      </div>
+    </main>
+  );
+}
